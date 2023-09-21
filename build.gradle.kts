2ac322e77f34886b1134a82b913d4f4db74dfded
byquanton <32410361+byquanton@users.noreply.github.com>
2023-09-21 22:32:28 +0200

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+    id("xyz.jpenilla.run-paper") version "2.2.0"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+}
+
+group = "com.kalimero2.team"
+version = "1.0.0"
+description = "Plugin Template"
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper", "paper-api", "1.20.1-R0.1-SNAPSHOT")
+    bukkitLibrary("cloud.commandframework", "cloud-paper", "1.8.4")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+bukkit {
+    main = "com.kalimero2.team.template.TemplatePlugin"
+    apiVersion = "1.20"
+    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
+    authors = listOf("Author")
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.20.1")
+    }
+}
